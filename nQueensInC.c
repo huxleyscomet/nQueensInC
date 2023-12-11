efedce42ad82afe0e3df30e0d7ac785765e9fe5f
huxleyscomet <109283919+huxleyscomet@users.noreply.github.com>
2023-12-11 18:30:22 +0300

    Update nQueensInC.c

--- nQueensInC.c
+++ nQueensInC.c
@@ -2,15 +2,18 @@
 #include <stdbool.h>
 int main() {
 	bool condition = true;
-	int queensX[9];
-	int queensY[9] = {1,2,3,4,5,6,7,8,9};
+	int queensX[8];
+	int queensY[8] = {1,2,3,4,5,6,7,8};
 	printf("Please enter the x coordinates of the queens (starting from the top)> ");
-	for (int i = 0; i < 9; i++) {
+	for (int i = 0; i < 8; i++) {
 		scanf("%d", &queensX[i]);
 	}
-	for (int i = 0; i < 9; i++) {
-		for (int l = i+1; l < 8; l++) {
-			if (((queensX[i] + queensX[l]) == (queensY[i] + queensY[l])) || (queensX[i] == queensX[l])) {
+	for (int i = 0; i < 8; i++) {
+		for (int l = i+1; l < 7; l++) {
+			if (((queensX[i] + queensX[l]) == (queensY[i] + queensY[l])) || 
+				(queensX[i] == queensX[l]) || 
+				(queensY[i] == queensY[l]) || 
+				((queensX[i]- queensX[l]) == (queensY[i] - queensY[l]))) {
 				condition = false;
 				printf("(%d,%d) \t (%d,%d)\n", queensX[i], queensY[i], queensX[l + 1], queensY[l + 1]);
 			}
@@ -19,9 +22,9 @@ int main() {
 		i++;
 	}
 	if (condition == true) {
-		printf("The grid is in the correct condition.");
+		printf("The grid is in a correct state.");
 	}
 	else {
-		printf("The grid is in the incorrect position.");
+		printf("The grid is in an incorrect state.");
 	}
 }
